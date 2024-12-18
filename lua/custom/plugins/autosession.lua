31e7a46aa0f763876af10fcd0a43f0b6aeb15ba9
Mateus L. Nascimento <mateus.loures@iebtinnovation.com>
2024-12-18 14:51:48 -0300

    add autosession plugin

---
+++ lua/custom/plugins/autosession.lua
@@ -0,0 +1,15 @@
+return {
+
+    {
+        'rmagatti/auto-session',
+        lazy = false,
+        -- config = function()
+        --     require('auto-session').setup()
+        -- end,
+        ---@module "auto-session"
+        ---@type AutoSession.Config
+        opts = {
+            -- auto_restore_last_session = true,
+        }
+    },
+}
